bb1a4ba9be12878ee23507b23e5f5121cd346d96
abstractmachines <abstractmachines@gmail.com>
2024-11-19 20:01:06 -0800

    Poisson distribution Devore 3.81.

--- poisson-cumulative-devore-3.81.R
+++ poisson-cumulative-devore-3.81.R
@@ -11,4 +11,19 @@ mu <- 20
 p_at_most_10 <- ppois(q = 10, lambda = mu)
 
 # Print the result
-print(p_at_most_10)
+print(p_at_most_10) # 0.01081172 -> part (a)
+
+probability_less_equal_20 <- ppois(q = 20, lambda = mu)
+probability_greater_20 <- 1 - probability_less_equal_20
+print(probability_greater_20) # 0.4409074 -> part (b)
+
+probability_between_10_and_20 <- ppois(q = 20, lambda = mu) - ppois(q = 9, lambda = mu)
+
+# Print the result
+print(probability_between_10_and_20) # 0.5540972 (also .459 per Devore) -> part (c)
+
+std_dev <- sqrt(mu)
+lower_bound <- mu - 2 * std_dev
+upper_bound <- mu + 2 * std_dev
+within_2_std_dev <- ppois(q = upper_bound, lambda = mu) - ppois(q = lower_bound - 1, lambda = mu)
+print(within_2_std_dev) # 0.9548548 -> part (d)

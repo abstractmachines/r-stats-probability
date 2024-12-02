50e61dd9e065edc5d6acd790d403b60564753acf
abstractmachines <abstractmachines@gmail.com>
2024-12-02 11:50:07 -0800

    R standard normal distribution work w pnorm().

---
+++ standard-normal-distribution.R
@@ -0,0 +1,16 @@
+# Devore 4.32 Standard Normal Distribution
+
+# P(Z <= mu):
+pnorm(0) # 0.5
+
+# P(Z <= 2):
+pnorm(2) # 0.9772
+
+# P(Z <= 4):
+pnorm(4) # 0.999
+
+#P(2.4) - P(-0.8)
+pnorm(2.4) - pnorm(-0.8) # 0.7799
+
+#P(2.4) - P(-2.4)
+pnorm(2.4) - pnorm(-2.4) # 0.9836

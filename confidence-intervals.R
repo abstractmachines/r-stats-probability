f5d8c07c71eebcc7c0d39bcc3d520175e4217953
abstractmachines <abstractmachines@gmail.com>
2025-01-29 19:55:02 -0800

    Fix CI intervals formula.

--- confidence-intervals.R
+++ confidence-intervals.R
@@ -3,11 +3,12 @@
 ci <- 0.9
 sample_mean = 8.393
 n = 86
+sigma = 0.1
 
 # Static values and computations  
 alpha <- 1-ci
 z_alpha_2 <- qnorm(1-alpha/2)
-bounds <- (z_alpha_2 * (alpha/sqrt(n)))
+bounds <- (z_alpha_2 * (sigma/sqrt(n)))
 print(bounds)
 
 lower_bound = sample_mean - bounds

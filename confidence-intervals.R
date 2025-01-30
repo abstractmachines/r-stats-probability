5e4624d1dd2bdf53a29528ddc89dff5ecd2044e4
abstractmachines <abstractmachines@gmail.com>
2025-01-29 19:38:57 -0800

    R example of confidence intervals.

---
+++ confidence-intervals.R
@@ -0,0 +1,17 @@
+
+#givens
+ci <- 0.9
+sample_mean = 8.393
+n = 86
+
+# Static values and computations  
+alpha <- 1-ci
+z_alpha_2 <- qnorm(1-alpha/2)
+bounds <- (z_alpha_2 * (alpha/sqrt(n)))
+print(bounds)
+
+lower_bound = sample_mean - bounds
+upper_bound = sample_mean + bounds
+print (lower_bound)
+
+cat("We are ", ci*100, "percent confident that the true statistic is between ", lower_bound, " and ", upper_bound)

1b788ef2499675a336f4f093ed520a9d17b030e4
abstractmachines <abstractmachines@gmail.com>
2025-03-03 19:37:07 -0800

    plotting, but need critical (rejection) regions and a curve.

--- hypothesis-testing.R
+++ hypothesis-testing.R
@@ -1,8 +1,8 @@
 # one sample t tests, t distribution
-x2 <- c(80,68,72,73,76,81,71,71,65,50,63,71,70,76,75,69,70,72,74)
-t.test(x2,conf.level=0.01) #gives us mean, ci, and p value...
-sample_mean <- mean(x2)
-sample_deviation <- sd(x2)
+x <- c(80,68,72,73,76,81,71,71,65,50,63,71,70,76,75,69,70,72,74)
+t.test(x,conf.level=0.01) #gives us mean, ci, and p value...
+sample_mean <- mean(x)
+sample_deviation <- sd(x)
 known_mu <- 75
 n <- 19
 
@@ -17,4 +17,12 @@ print(t_critical_value) # 2.88
 
 # Therefore, since our "hit" is outside of the rejection region
 # for the null hypothesis -> "null hypothesis is true" ("fail to reject H0.")
+
+#create a vector of values that shows the height of the probability distribution
+#for each value in x, using degrees of freedom
+y <- dt(x = x, df = n-1)
+
+#plot x and y
+plot(x,y)
+
 

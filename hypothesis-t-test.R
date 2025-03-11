862858b972563131cf9a09418574ee0307817953
abstractmachines <abstractmachines@gmail.com>
2025-03-11 11:34:30 -0700

    Correction: instead of using p values to reject or not reject H0, instead use simple rejection regions by comparing test statistic to critical values.

--- hypothesis-t-test.R
+++ hypothesis-t-test.R
@@ -1,7 +1,19 @@
 # Hypothesis testing
 
 n = 58 # but sigma unknown, so use t distribution as such
-qt(1-0.001,df=57)
+qt(1-0.001,df=57) # 3.239478 -> critical value,
+x_bar <- 191 # sample mean
+mu <- 153 # population mean
+# Since n > 30, we are safe using sample statistics (sample mean, sample std dev s...)
+s <- 89
+t_test_statistic <- (x_bar - mu)/(s/(sqrt(n))) 
+print(t_test_statistic) # 3.25
+
+# Since this is a right tailed test, and critical value is 3.23,
+# where critical value defines the rejection regions,
+# then the test statistic of 3.25 hits within that rejection region for H0,
+# we then reject the null hypothesis of H0.
+
 # this is a right tailed test, so
 p_value <- pt(3.25,df = n-1, lower.tail = FALSE)
 print(p_value)
